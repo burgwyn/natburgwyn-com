011f27b9ac3b7a9872c5a1812eab692b1036a1a2
Nat Burgwyn <burgwyn@gmail.com>
2020-10-31 22:41:52 -0400

    Add new component and test

---
+++ src/components/Tag.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Tag from './Tag';
+
+describe('Tag', () => {
+    it('renders', () => {
+        const tag = renderer.create(<Tag tag={'tag'} />);
+        expect(tag).toBeDefined();
+    });
+});
